3fe4e87f9a1468ad6b84247c57bc4aaa8df4d00a
Andreea <andrex_97@yahoo.com>
2021-01-25 22:56:53 +0200

    refactor sql scripts

---
+++ src/main/resources/creare_user.sql
@@ -0,0 +1,6 @@
+CREATE USER oltp_admin IDENTIFIED BY parola;
+GRANT CREATE SESSION TO oltp_admin;
+GRANT CREATE TABLE TO oltp_admin;
+GRANT SELECT ANY SEQUENCE TO oltp_admin;
+GRANT CREATE ANY SEQUENCE TO oltp_admin;
+ALTER USER oltp_admin quota 50M on USERS;
---
+++ src/main/resources/drop-tabele-oltp.sql
@@ -0,0 +1,24 @@
+--pt drop
+-- drop table Institutie_Profesor;
+drop table Clasa_Curs_Profesor;
+drop table Profesor_Specializare;
+drop table Specializare_Didactica;
+drop table profesor;
+drop table nota;
+drop table istoric;
+drop table elev;
+drop table curs;
+
+drop table clasa;
+drop table specializare;
+drop table Profil;
+drop table Institutie_Invatamant;
+drop table Tip_Institutie_Invatamant;
+drop table adresa;
+
+drop table localitate;
+drop table judet;
+drop table subregiune;
+drop table Regiune;
+drop table Tip_Zona;
+
